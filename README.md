f5304407308bb9a4369df166e6fb58c62cd68f46
JackWong <wangchangat@163.com>
2020-07-12 22:11:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ipinfo
+
+
+
+[ipinfo]: https://ipinfo.io	"里面使用的我自己的api token"
+
+根据 ipinfo 的api接口,返回对应的ip相关信息
+
+
+
+###### 具体格式如下:
+
+```json
+ {
+  "ip": "172.104.164.210",
+  "hostname": "li1755-210.members.linode.com",
+  "city": "Singapore",
+  "region": "Singapore",
+  "country": "SG",
+  "loc": "1.2897,103.8501",
+  "org": "AS63949 Linode, LLC",
+  "postal": "048508",
+  "timezone": "Asia/Singapore"
+}
+```
+
---
+++ ipinfo.go
@@ -0,0 +1,43 @@
+package ipinfo
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type IpJson struct {
+	IP       string `json:"ip"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Timezone string `json:"timezone"`
+}
+
+func GetIp() IpJson{
+	defer func() {
+		recover()
+	}()
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	req, _ := http.NewRequest("GET", "https://ipinfo.io", nil)
+	req.Header.Set("Authorization", "Bearer 42b543091592e3")
+	resp, err := client.Do(req)
+
+	if err == nil {
+		defer resp.Body.Close()
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			log.Printf("%s",body)
+			ipJsonObj := IpJson{}
+			json.Unmarshal(body, &ipJsonObj)
+			return ipJsonObj
+		}
+	}
+	return IpJson{}
+}
